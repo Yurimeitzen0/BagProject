17025a5b73585004818a6e4e9c833ad2a59a6a8b
WELTALL\Yuri.Meitzen <yuri.meitzen@gmail.com>
2018-02-27 00:48:48 -0300

    Added Fire Recognition, Still Implementing avoid method.

---
+++ AiGame/Intermediate/Build/Win64/AiGame/Development/UELinkerFixups/PCH.UELinkerFixups.cpp
@@ -0,0 +1,1 @@
+#include "D:/MyDocuments/Unreal Projects/BagProject/AiGame/Intermediate/Build/Win64/AiGame/Development/UELinkerFixups/PCH.UELinkerFixups.h"
---
+++ AiGame/Intermediate/Build/Win64/AiGame/Development/UELinkerFixups/PCH.UELinkerFixups.h
@@ -0,0 +1,2 @@
+// PCH for D:\MyDocuments\Unreal Projects\BagProject\AiGame\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h
+#include "D:\MyDocuments\Unreal Projects\BagProject\AiGame\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
